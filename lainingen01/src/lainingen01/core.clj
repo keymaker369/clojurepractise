ab3d72c39e2c444d72dabbec0d095ec2f69acd8d
seke.nenad <seke.nenad@3b9a4193-d2c2-5bc0-5570-ecbec313bbc1>
2012-10-03 15:40:47 +0000

    git-svn-id: https://clojurepractise.googlecode.com/svn/trunk@3 3b9a4193-d2c2-5bc0-5570-ecbec313bbc1

---
+++ lainingen01/src/lainingen01/core.clj
@@ -0,0 +1,25 @@
+(ns lainingen01.core)
+
+
+(print-all-things '(test-string test-string2))
+(count '(test-string test-string2))
+
+(defn print-all-things [a-list-of-things]
+  (let [total (count a-list-of-things)]
+     (println "Total things: " total)
+     (dorun (map println a-list-of-things))))
+
+
+(def test-string (new String "test stringnn"))
+(def test-string2 (.concat test-string " 2"))
+
+
+(println test-string)
+(println test-string2)
+(println (.length test-string))
+
+
+(defn -main
+  "I don't do a whole lot."
+  [& args]
+  (println "Hello, World!"))
