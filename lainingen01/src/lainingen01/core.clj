e793955cf73da0939b5819782698a23ef140ca6b
seke.nenad <seke.nenad@3b9a4193-d2c2-5bc0-5570-ecbec313bbc1>
2013-01-28 23:23:45 +0000

    git-svn-id: https://clojurepractise.googlecode.com/svn/trunk@15 3b9a4193-d2c2-5bc0-5570-ecbec313bbc1

--- lainingen01/src/lainingen01/core.clj
+++ lainingen01/src/lainingen01/core.clj
@@ -1,54 +1,118 @@
-(ns lainingen01.core)
+(ns lainingen01.core
+  (:gen-class :main true)
+  (:use seesaw.core)
+  (:use seesaw.border))
 
+(require 'clojure.java.io)
+(defn load-props
+  [file-name]
+  (with-open [^java.io.Reader reader (clojure.java.io/reader file-name)] 
+    (let [props (java.util.Properties.)]
+      (.load props reader)
+      (into {} (for [[k v] props] [(keyword k) (read-string v)])))))
 
+;refovi
+(def recnik (load-props "//c://recnik.properties"))
+(def r-not-used (ref recnik))
+(def r-odabran (ref {}))
+(def r-used (ref {}))
 
+;gui
+(def f (frame :title "Language helpersss"  :size [400 :by 400] 
+              :content (canvas :id :canvas :background "#BBBBBB")))
+(def word (text :multi-line? false :font "MONOSPACED-PLAIN-14" :text "guessing text"))
+(def result (text :multi-line? true :font "MONOSPACED-PLAIN-14" :text "result"))
+(def b1 (button :text "Click Me1"))
+(def b2 (button :text "Click Me2"))
+(def b3 (button :text "Click Me3"))
+(def b4 (button :text "Click Me4"))
+(def b5 (button :text "Click Me5"))
 
-(defn uradi-dec [input1, brojRedova]	
-	(loop [x brojRedova]
-	     (if (neg? x)
-	     x
-	     (do 
-	       (println (.concat input1 (.toString x)))
-	       (recur (dec x))))))
+(defn display [content]
+  (content)
+  content)
+	                             
+(config! f :content (border-panel
+           :north word
+           :center (vertical-panel :items (list b1 b2 b3 b4 b5))
+           :south result
+           :vgap 5 :hgap 5 :border 5))
 
-(defn uradi-inc [input1, brojRedova]	
-  (loop [x 2]
-    (if (= x brojRedova)
+;funkcije
+(defn select-random
+    "Returns a random item from a list of items."
+    [options]
+    (if (> (count options) 0)
+    (nth options (rand-int (count options)))
+    nil))
+
+(defn pick-random-amount 
+  "Picks some amount of elements from list"
+  ([lista amount] 
+    (pick-random-amount lista amount (list)))
+  ([lista amount rezultat ]
+	  (let [picked-elements rezultat
+	        odabran (select-random lista)]
+	    (if (= amount (count picked-elements))
+	    picked-elements
+     (do
+	    (recur
+       (remove #{odabran} lista)
+       amount
+	     (conj picked-elements odabran)))))))
+
+(defn add-to-map-ref [map-ref key val]
+  (dosync (ref-set map-ref (assoc @map-ref key val))))
+
+(defn remove-from-map-ref [map-ref key]
+  (dosync (ref-set map-ref (dissoc @map-ref key))))
+
+(defn show []
+  (let [five-keys (pick-random-amount (keys @r-not-used) 
+                                      (if (> (.size @r-not-used) 5) 5 (.size @r-not-used)))
+        random-key (select-random five-keys)]
+    (config! word :text (name random-key))
+    (add-to-map-ref r-odabran random-key (recnik random-key))
+    (remove-from-map-ref r-not-used random-key)
+    (loop [keys five-keys buttons (list b1 b2 b3 b4 b5)]
+      (if (= 0 (.size keys))
       nil
-	    (do 
-       (spit "C:\\Users\\nikola\\workspace\\filmania\\src\\ets\\dataset.csv" 
-             (str input1 x "\n") 
-             :append true)
-	     (recur (inc x))))))
+      (do
+        (config! (first buttons) :text (name (recnik (first keys))))
+        (recur (remove #{(first keys)} keys) (remove #{(first buttons)} buttons))
+        )))
+    ))
 
+(defn uradi [answer]
+  (println (.toString answer))
+  (let [result-key (first (keys @r-odabran))]
+    (println (.toString (recnik result-key)))
+    (if (.equals (.toString answer) (.toString (recnik result-key)))
+      (do
+        (println "tacno")
+        (config! result :text "tacno")
+        (show))
+      (do
+        (println "netacno")
+        (config! result :text "netacno")
+        (remove-from-map-ref r-odabran result-key)
+        (add-to-map-ref r-used result-key (recnik result-key))
+        (show))))) 
 
-(uradi-inc "" 1100)
-
-
-(spit "blubber.txt" "11111test\n" :append true)
-(slurp "blubber.txt")
-(println (slurp "blubber.txt"))
-
-
-(print-all-things '(test-string test-string2))
-(count '(test-string test-string2))
-
-(defn print-all-things [a-list-of-things]
-  (let [total (count a-list-of-things)]
-     (println "Total things: " total)
-     (dorun (map println a-list-of-things))))
-
-
-(def test-string (new String "test stringnn"))
-(def test-string2 (.concat test-string " 2"))
-
-
-(println test-string)
-(println test-string2)
-(println (.length test-string))
-
+(listen b1 :action (fn [e] (uradi (.getText (.getSource e)))))
+(listen b2 :action (fn [e] (uradi (.getText (.getSource e)))))
+(listen b3 :action (fn [e] (uradi (.getText (.getSource e)))))
+(listen b4 :action (fn [e] (uradi (.getText (.getSource e)))))
+(listen b5 :action (fn [e] (uradi (.getText (.getSource e))))) 
 
 (defn -main
   "I don't do a whole lot."
   [& args]
-  (println "Hello, World!"))
+  (do
+    (-> f pack! show!)
+    (show)))
+
+;(listen b1 :mouse-entered #(config! % :foreground :black)
+;          :mouse-exited  #(config! % :foreground :black))
+;(*1)
+
---
+++ lainingen01/src/lainingen01/core1.clj
@@ -0,0 +1,58 @@
+(ns lainingen01.core1)
+
+
+
+
+(defn uradi-dec [input1, brojRedova]	
+	(loop [x brojRedova]
+	     (if (neg? x)
+	     x
+	     (do 
+	       (println (.concat input1 (.toString x)))
+	       (recur (dec x))))))
+
+(defn uradi-inc [input1, brojRedova]	
+  (loop [x 2]
+    (if (= x brojRedova)
+      nil
+	    (do 
+       (spit "/home/user/workspace/filmania/src/ets/dataset.csv" 
+             (str input1 x "\n") 
+             :append true)
+	     (recur (inc x))))))
+
+
+;(uradi-inc "" 99)
+
+
+(spit "blubber.txt" "11111test\n" :append true)
+(slurp "blubber.txt")
+(println (slurp "blubber.txt"))
+
+
+(print-all-things '(test-string test-string2))
+(count '(test-string test-string2))
+
+(defn print-all-things [a-list-of-things]
+  (let [total (count a-list-of-things)]
+     (println "Total things: " total)
+     (dorun (map println a-list-of-things))))
+
+
+(def test-string (new String "test stringnn"))
+(def test-string2 (.concat test-string " 2"))
+
+
+(println test-string)
+(println test-string2)
+(println (.length test-string))
+
+
+(defn -main
+  "I don't do a whole lot."
+  [& args]
+  (println "Hello, World!"))
+
+
+
+
