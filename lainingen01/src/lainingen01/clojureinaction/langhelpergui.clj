162ae4c8fb067f62fa13eb05caed6e25b37cea40
seke.nenad <seke.nenad@3b9a4193-d2c2-5bc0-5570-ecbec313bbc1>
2013-01-28 23:23:37 +0000

    git-svn-id: https://clojurepractise.googlecode.com/svn/trunk@14 3b9a4193-d2c2-5bc0-5570-ecbec313bbc1

--- lainingen01/src/lainingen01/clojureinaction/langhelpergui.clj
+++ lainingen01/src/lainingen01/clojureinaction/langhelpergui.clj
@@ -2,10 +2,48 @@
   (:use seesaw.core)
   (:use seesaw.border))
 
+(require 'clojure.java.io)
+(defn load-props
+  [file-name]
+  (with-open [^java.io.Reader reader (clojure.java.io/reader file-name)] 
+    (let [props (java.util.Properties.)]
+      (.load props reader)
+      (into {} (for [[k v] props] [(keyword k) (read-string v)])))))
+
+;refovi
+(def recnik (load-props "//c://recnik.properties"))
+(def r-not-used (ref recnik))
+(def r-odabran (ref {}))
+(def r-used (ref {}))
+
+;gui
+(def f (frame :title "Language helpersss"  :size [400 :by 400] 
+              :content (canvas :id :canvas :background "#BBBBBB")))
+(def word (text :multi-line? false :font "MONOSPACED-PLAIN-14" :text "guessing text"))
+(def result (text :multi-line? true :font "MONOSPACED-PLAIN-14" :text "result"))
+(def b1 (button :text "Click Me1"))
+(def b2 (button :text "Click Me2"))
+(def b3 (button :text "Click Me3"))
+(def b4 (button :text "Click Me4"))
+(def b5 (button :text "Click Me5"))
+
+(defn display [content]
+  (config! f :content content)
+  content)
+	                             
+(display (border-panel
+           :north word
+           :center (vertical-panel :items (list b1 b2 b3 b4 b5))
+           :south result
+           :vgap 5 :hgap 5 :border 5))
+
+;funkcije
 (defn select-random
     "Returns a random item from a list of items."
     [options]
-    (nth options (rand-int (count options))))
+    (if (> (count options) 0)
+    (nth options (rand-int (count options)))
+    nil))
 
 (defn pick-random-amount 
   "Picks some amount of elements from list"
@@ -14,88 +52,67 @@
   ([lista amount rezultat ]
 	  (let [picked-elements rezultat
 	        odabran (select-random lista)]
-	    (if (= amount (.size picked-elements))
+	    (if (= amount (count picked-elements))
 	    picked-elements
+     (do
 	    (recur
        (remove #{odabran} lista)
        amount
-	     (conj picked-elements odabran))))))
+	     (conj picked-elements odabran)))))))
 
 (defn add-to-map-ref [map-ref key val]
-  (dosync (ref-set odabran (assoc @map-ref key val))))
+  (dosync (ref-set map-ref (assoc @map-ref key val))))
 
-(def f (frame :title "Language helpersss"  :size [400 :by 400] 
-              :content (canvas :id :canvas :background "#BBBBBB")))
-(defn display [content]
-  (config! f :content content)
-  content)
+(defn remove-from-map-ref [map-ref key]
+  (dosync (ref-set map-ref (dissoc @map-ref key))))
 
-(def word (text :multi-line? false :font "MONOSPACED-PLAIN-14" :text "guessing text"))
-(def result (text :multi-line? true :font "MONOSPACED-PLAIN-14" :text "result"))
-(def b1 (button :text "Click Me1"))
-(def b2 (button :text "Click Me2"))
-(def b3 (button :text "Click Me3"))
-(def b4 (button :text "Click Me4"))
-(def b5 (button :text "Click Me5"))
-                             
-(display (border-panel
-           :north word
-           :center (vertical-panel :items (list b1 b2 b3 b4 b5))
-           :south result
-           :vgap 5 :hgap 5 :border 5))
-
-(def src {"1" "1111" "22" "2222" "3" "3333" "4" "4444" "5" "5555" "6" "6666"})
-(def not-used (ref src))
-(def odabran (ref {}))
-(def used (ref {}))
-
-(do
-  (-> f pack! show!)
-  (let [five-keys (pick-random-amount (keys @not-used) 5)
+(defn show []
+  (let [five-keys (pick-random-amount (keys @r-not-used) 
+                                      (if (> (.size @r-not-used) 5) 5 (.size @r-not-used)))
         random-key (select-random five-keys)]
-    (config! word :text random-key)
-    (add-to-map-ref odabran random-key (src random-key))))
-  
-(config! word :text (select-random (pick-random-amount (keys @not-used) 5)))
+    (config! word :text (name random-key))
+    (add-to-map-ref r-odabran random-key (recnik random-key))
+    (remove-from-map-ref r-not-used random-key)
+    (loop [keys five-keys buttons (list b1 b2 b3 b4 b5)]
+      (if (= 0 (.size keys))
+      nil
+      (do
+        (config! (first buttons) :text (name (recnik (first keys))))
+        (recur (remove #{(first keys)} keys) (remove #{(first buttons)} buttons))
+        )))
+    ))
 
-(listen b1 :action (fn [e] (println (.getText (.getSource e)))))
-(listen b2 :action (fn [e] (println (.getText (.getSource e)))))
-(listen b3 :action (fn [e] (println (.getText (.getSource e)))))
-(listen b4 :action (fn [e] (println (.getText (.getSource e)))))
-(listen b5 :action (fn [e] (println (.getText (.getSource e)))))
+(defn uradi [answer]
+  (println (.toString answer))
+  (let [result-key (first (keys @r-odabran))]
+    (println (.toString (recnik result-key)))
+    (if (.equals (.toString answer) (.toString (recnik result-key)))
+      (do
+        (println "tacno")
+        (config! result :text "tacno")
+        (show))
+      (do
+        (println "netacno")
+        (config! result :text "netacno")
+        (remove-from-map-ref r-odabran result-key)
+        (add-to-map-ref r-used result-key (recnik result-key))
+        (show))))) 
 
-(do
-  (config! result :text (src (first (keys @odabran))))
+(listen b1 :action (fn [e] (uradi (.getText (.getSource e)))))
+(listen b2 :action (fn [e] (uradi (.getText (.getSource e)))))
+(listen b3 :action (fn [e] (uradi (.getText (.getSource e)))))
+(listen b4 :action (fn [e] (uradi (.getText (.getSource e)))))
+(listen b5 :action (fn [e] (uradi (.getText (.getSource e))))) 
 
+(defn -main
+  "I don't do a whole lot."
+  [& args]
+  (do
+    (-> f pack! show!)
+    (show)))
 
-
-(add-to-map-ref odabran :2 1111)
-
-(pick-random-amount (keys src) 3)
-(remove #{:1} (keys src))
-(defn ddd [a & more] (println))
-
-(ddd 3 4)
-
-(listen b1 :action (fn [e] (config! b1 :text "ddd")))
 
 ;(listen b1 :mouse-entered #(config! % :foreground :black)
 ;          :mouse-exited  #(config! % :foreground :black))
-
 ;(*1)
-
-(def my-ref (ref 5))
-
-(dosync (ref-set my-ref 6))
-
-@mymap
-
-(config! b2 :text "ddd")
-(config! word :text "ddd")
-
-(doc text)
-
-;(def mapa {:11 22 :333 444})
-
-;(get mapa :11) 
 
