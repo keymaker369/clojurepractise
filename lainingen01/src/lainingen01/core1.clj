3d5e8302f7cb9c37c50f7e7a6eafd99381294fb4
seke.nenad <seke.nenad@3b9a4193-d2c2-5bc0-5570-ecbec313bbc1>
2013-02-26 17:06:36 +0000

    git-svn-id: https://clojurepractise.googlecode.com/svn/trunk@20 3b9a4193-d2c2-5bc0-5570-ecbec313bbc1

--- lainingen01/src/lainingen01/core1.clj
+++ lainingen01/src/lainingen01/core1.clj
@@ -11,19 +11,54 @@
 	       (println (.concat input1 (.toString x)))
 	       (recur (dec x))))))
 
-(defn uradi-inc [input1, brojRedova]	
-  (loop [x 2]
-    (if (= x brojRedova)
-      nil
-	    (do 
-       (spit "/home/user/workspace/filmania/src/ets/dataset.csv" 
-             (str input1 x "\n") 
-             :append true)
-	     (recur (inc x))))))
+(defn uradi-inc [text, broj-redova, start-from, append, formater, parameters]	
+  (let [buffer (new StringBuffer)]
+	  (loop [x start-from]
+	    (if (= x broj-redova)
+	      (do 
+         ;(println ;spit (str "d:/posao-sheme/" text "s.csv") 
+          ;     (.toString buffer) 
+           ;    );:append append)
+         x)
+       (do 
+         (.append buffer  
+                  (formater x text parameters))
+         (recur (inc x)))))))
+
+(defn venue-formater [x text parameters]
+  (str x "," x "," text x "," text x "," 0 "," 0 "," 0 "," "2012-04-20 14:57:22" "," "2012-04-20 14:57:22" "," "2012-04-20 14:57:22\n"))
+
+(defn event-formater [x text parameters]
+  (let [venue-id (first parameters)]
+    (str x "," venue-id x "," text x "," text x ",0,0,1," venue-id ", 2012-10-18 12:37:00, 2012-10-18 14:37:00, 0, 0, 1, 0, 1, 1, 1, 1,2012-04-20 14:57:22,2012-04-20 14:57:22,2012-04-20 14:57:22\n")))
 
 
-;(uradi-inc "" 99)
+(uradi-inc "venue" 201 3 false venue-formater nil)
 
+
+;(loop [x 1 id-brojac 1]
+;  (if (= x 3)
+;    nil
+;      (recur (inc x) (inc (uradi-inc "event" 51 id-brojac true event-formater (list x))))))
+
+(loop [x 1 id-brojac 1]
+  (if (= x 5)
+    (println x id-brojac)
+    (do
+      (println x id-brojac)
+      (recur (inc x) (petlja id-brojac)))))
+
+(defn petlja [x]
+  (let [max (+ x 3)]
+  (loop [x 1]
+    (if (= x max)
+      x
+      (recur (inc x))))))
+
+(petlja 6)
+
+
+(uradi-inc "event" 51 1 false event-formater '(1))
 
 (spit "blubber.txt" "11111test\n" :append true)
 (slurp "blubber.txt")
