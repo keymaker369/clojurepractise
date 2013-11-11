9294596437e95b2390b871d6cd4b82e6706df865
seke.nenad@gmail.com <seke.nenad@gmail.com@3b9a4193-d2c2-5bc0-5570-ecbec313bbc1>
2013-11-11 18:10:31 +0000

    Added directive where is main method to run.
    git-svn-id: https://clojurepractise.googlecode.com/svn/trunk@21 3b9a4193-d2c2-5bc0-5570-ecbec313bbc1

--- lainingen01/project.clj
+++ lainingen01/project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/math.numeric-tower "0.0.1"]
-                 [seesaw "1.4.2"]])
+                 [seesaw "1.4.2"]]
+  :main lainingen01.core)
