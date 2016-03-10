b230adf539ab1f087a8166133cd55b332f017047
key <seke.nenad@gmail.com>
2016-03-10 00:58:00 -0800

    intense

--- project.clj
+++ project.clj
@@ -3,12 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/math.combinatorics "0.1.1"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [seesaw "1.4.5"]
                  [clj-systemtray "0.2.1"]
                  [overtone/at-at "1.2.0"]
-                 [enlive "1.1.6"]]
+                 [enlive "1.1.6"]
+                 [net.mikera/imagez "0.10.0"]]
   :plugins [[lein-eclipse "1.0.0"]]
   :main lainingen01.core)
--- src/lainingen01/intense.clj
+++ src/lainingen01/intense.clj
@@ -1,5 +1,10 @@
 (ns lainingen01.intense
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str])
+  (:use mikera.image.colours)
+  (:use mikera.image.core)
+  (:use mikera.image.filters)
+  (:use mikera.image.spectrum)
+  (:require [clojure.java.io :refer [resource]]))
 
 (def numbers '(0 1 2 3 4 5 6 7 8 9))
 (def low-letters '("a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z"))
@@ -12,10 +17,19 @@
 
 (count (all-characters))
 
-(loop [x 0]
-  (if (= x (count (all-characters)))
-    (str "Kraj " (java.lang.Thread/currentThread))
-    (do
-      (println 
-        (nth (all-characters) x))
-      (recur (inc x)))))
+(defn do-something-intense []
+  (println 
+    (str (java.lang.Thread/currentThread) (new java.util.Date)))
+	(loop [x 0]
+	  (if (= x 100)
+	    (str "Kraj " (java.lang.Thread/currentThread) (new java.util.Date))
+	    (do
+	      (->
+	       (load-image "c:/Users/user/Downloads/wallpapers/infinite-crisis-15456.jpg")
+				  (flip :vertical)
+				  ((invert))
+				  ((brightness 2.0))
+				  ((contrast 0.5)))
+	      (recur (inc x))))))
+
+
